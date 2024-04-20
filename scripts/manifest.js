eb4bee8ea84d64d228366ef6ca612c9b623b0f5b
51breeze <664371281@qq.com>
2024-04-20 13:51:01 +0800

    fix

--- scripts/manifest.js
+++ scripts/manifest.js
@@ -1,16 +1,12 @@
 const path = require('path');
 const Compiler = require("easescript/lib/core/Compiler");
-
 Compiler.buildTypesManifest(
-    [path.resolve('./types/index.d.es')], 
+    [
+        path.resolve('./types/index.d.es')
+    ], 
     {
         name:'es-nuxt', 
         inherits:['es-vue','es-javascript']
     },
-    path.join(__dirname,'../types'),
-    {compilerOptions:{
-        debug:true,
-        configFileName:null,
-        cwd:path.join(__dirname, '../')
-    }}
+    './types'
 );
